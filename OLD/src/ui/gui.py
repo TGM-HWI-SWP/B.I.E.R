a05d76d9e28ea4e2faf57600bc775b466afd6bb1
PHinterbauer <phinterbauer.privat@gmail.com>
2026-02-25 14:15:04 +0100

    finally removed this so called "template"

--- OLD/src/ui/gui.py
+++
@@ -1,9 +0,0 @@
-"""UI Layer - Gradio Web Interface"""
-
-import gradio as gr
-
-with gr.Blocks(title="Lagerverwaltung") as app:
-    gr.Markdown("# Lagerverwaltung – Bürobedarf")
-    gr.Markdown("🚧 Businesslogik wird noch implementiert.")
-
-app.launch(server_name="0.0.0.0", server_port=7860)
