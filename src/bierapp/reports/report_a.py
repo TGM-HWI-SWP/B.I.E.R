9ad94c348c3654c0df49c75dce9fcd798b3400f4
PHinterbauer <phinterbauer.privat@gmail.com>
2026-04-08 11:46:33 +0200

    moved reports folder

--- src/bierapp/reports/report_a.py
+++
@@ -1,7 +0,0 @@
-from matplotlib import pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-from ..contracts import ReportPort
-from ..backend.service import ProductService, WarehouseService, InventoryService
-
-class ReportA(ReportPort):
-    ...
---
+++ src/reports/report_b.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from ..bierapp.contracts import ReportPort
+from ..bierapp.backend.service import ProductService, WarehouseService, InventoryService
+
+class ReportB(ReportPort):
+    ...
